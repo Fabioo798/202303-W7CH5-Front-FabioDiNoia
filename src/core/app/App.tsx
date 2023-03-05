050948f4dfc0982499332893d4c9524283678a09
Fabio <fabiodn.798@gmail.com>
2023-03-06 00:13:23 +0100

    🚧 Update the rest of the proyect with new features

--- src/core/app/App.tsx
+++ src/core/app/App.tsx
@@ -1,9 +1,34 @@
-import React from 'react';
-
+import { useState } from "react";
+import { LoginForm } from "../../users/component/login/login";
+import Register from "../../users/component/register/register";
+import Profile from "../../users/component/users/users";
+import "./App.css";
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    setLoggedIn(true);
+  };
+
+  const handleLogout = () => {
+    setLoggedIn(false);
+  };
+
   return (
     <div className="App">
+      {!loggedIn && (
+        <div>
+          <Register handleLogin={handleLogin} />
+          <LoginForm handleLogin={handleLogin} />
+        </div>
+      )}
+      {loggedIn && (
+        <div>
+          <button onClick={handleLogout}>Logout</button>
+          <Profile />
+        </div>
+      )}
     </div>
   );
 }
--- src/core/store/store.ts
+++ src/core/store/store.ts
@@ -1,7 +1,9 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import { userReducer } from '../../users/reducer/user.reducer';
 
 export const store = configureStore({
   reducer: {
+    users: userReducer
   },
 });
 
